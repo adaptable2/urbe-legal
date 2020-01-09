4dcf8f0153e6b000734539674702f7ff2faffd43
Camilo Castañeda <jccastaneda82@misena.edu.co>
2020-01-09 16:40:16 -0500

    detalles en los textos de la biga y dimeciones de las columnas

--- index.php
+++ index.php
@@ -47,36 +47,37 @@
 			<div class="row">
 				<div class="col-md-3 barra mb-5 mb-md-0">
 					<div class="row text-center h-100">
-						<div class="offset-md-2 col-md-8 d-flex align-items-end justify-content-center border-item-t pt-5">
+						<div class="offset-md-2 col-md-8 offset-lg-3 col-lg-6 d-flex align-items-end justify-content-center border-item-t pt-5">
 							<p>abogados</p>
 						</div>
 						<div class="col-12 d-flex align-items-center justify-content-center d-flex align-items-end justify-content-center bg-celeste">
-							<h2>especializado</h2>
+							<h2>especializados</h2>
 						</div>
-						<div class="offset-md-2 col-md-8 border-item-x d-flex align-items-end justify-content-center pt-3">
-							<p>en procesis de</p>
+						<div class="offset-md-2 col-md-8 offset-lg-3 col-lg-6 border-item-x d-flex align-items-end justify-content-center pt-3">
+							<p>en procesos de</p>
 						</div>
-						<div class="offset-md-2 col-md-8 border-item-x d-flex align-items-center justify-content-center">
+						<div class="offset-md-2 col-md-8 offset-lg-3 col-lg-6 border-item-x d-flex flex-column align-items-center justify-content-center">
+							<span class="line"></span>
 							<h2>insilvencia econimica</h2>
 						</div>
-						<div class="offset-md-2 col-md-8 d-flex align-items-end justify-content-center border-item-b biga">
+						<div class="offset-md-2 col-md-8 offset-lg-3 col-lg-6 d-flex align-items-end justify-content-center border-item-b biga">
 							<img src="img/biga.svg" class="img-fluid">
 						</div>
 					</div>
 				</div>
-				<div class="col-md-8">
+				<div class="col-md-8 textos">
 					<div class="row">
 						<div class="col-md-6 text-center py-5 py-md-0">
-							<h3>
+							<h2>
 								erros más comunes En momentos de crisis
-							</h3>
+							</h2>
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel odio itaque hic sapiente pariatur nesciunt optio, suscipit accusamus veritatis temporibus veniam id autem molestiae. Rem quibusdam facere beatae dignissimos sequi.</p>
 							<a href="" class="btn-item">Descarga</a>
 						</div>
 						<div class="col-md-6 text-center py-5 py-md-0">
-							<h3>
+							<h2>
 								¿Qué hacer cuando se está en quiebra?
-							</h3>
+							</h2>
 							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem, ut minus? Numquam tempore deleniti id laboriosam, perspiciatis temporibus ea ullam quidem omnis, animi, modi aperiam voluptates. Quae maxime nulla autem!</p>
 							<a href="" class="btn-item">Descarga</a>
 						</div>
